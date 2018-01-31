2b343c54ccfd3f51bbc00eb9f7a2ea81e5de3371
Your Name <you@example.com>
2018-01-31 10:33:04 +0000

    run.sh with teardown and setup

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 set -eou pipefail
 
-docker run --env "DATABASE_URL=$DATABASE_URL" --restart always -p 5123:5000 -d beeline-routing:latest
+
+(docker stop beeline-routing && docker rm beeline-routing) || echo 'beeline-routing not started'
+
+docker run --name beeline-routing --env "DATABASE_URL=$DATABASE_URL" --restart always -p 5123:5000 -d beeline-routing:latest
