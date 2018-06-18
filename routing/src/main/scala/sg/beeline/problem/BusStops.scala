a775b3ba831ca23bc8fe8f2a19e08c3f8c690dbb
Daniel Sim <daniel_sim@data.gov.sg>
2018-06-18 11:21:55 +0800

    Delete BusStops.scala

--- routing/src/main/scala/sg/beeline/problem/BusStops.scala
+++
@@ -1,8 +0,0 @@
-package sg.beeline.problem
-
-case class BusStops(busStops: Seq[BusStop],
-                    distanceFunction: Function2[BusStop, BusStop, Double]) {
-  lazy val busStopsByIndex = Map(
-    busStops.map(b => (b.index, b)) : _*
-  )
-}
