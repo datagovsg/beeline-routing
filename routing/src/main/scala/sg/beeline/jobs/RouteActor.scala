263ae42fc0e0a6aa49a481bc1fd03b7bc1d23d96
daniel sim <daniel@LAPTOP-VIQA6AQL.localdomain>
2018-07-31 16:30:45 +0800

    force minimum of 3 processors?

--- routing/src/main/scala/sg/beeline/jobs/RouteActor.scala
+++ routing/src/main/scala/sg/beeline/jobs/RouteActor.scala
@@ -14,7 +14,10 @@ import scala.concurrent.ExecutionContext
 class RouteActor(dataSource: DataSource, suggestionSource: String => Seq[Suggestion]) extends Actor {
   // If we don't set this, Scalatest hangs when running multiple threads
   implicit val executionContext = ExecutionContext.fromExecutor(
-    new ForkJoinPool(Runtime.getRuntime.availableProcessors))
+    new ForkJoinPool(
+      scala.math.max(
+        3,
+        Runtime.getRuntime.availableProcessors)))
 
   private def routeFromRequest(suggestRequest: SuggestRequest) = suggestRequest match {
     case SuggestRequest(sLat, sLng, eLat, eLng, time, settings) =>
