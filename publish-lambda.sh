230cf5e521e27295441a5b0a198caf40c034f7cb
daniel sim <daniel@LAPTOP-VIQA6AQL.localdomain>
2018-08-30 14:49:12 +0800

    create lambda if it doesn't previously exist

--- publish-lambda.sh
+++ publish-lambda.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
+create_lambda () {
+  aws lambda create-function --function-name beeline-routing-suggestions \
+	--runtime java8 \
+	--role arn:aws:iam::882000534153:role/service-role/beeline-routing \
+	--handler 'sg.beeline.lambda.SuggestRouteHandler::handle' \
+	--code '{"S3Bucket":"beeline-routing","S3Key":"routing-lambda-assembly-1.0.jar"}'
+}
+
 sbt routingLambda/assembly
-aws s3 cp ./routing-lambda/target/scala-2.12/routing-lambda-assembly-1.0.jar s3://beeline-analytics/
-aws lambda update-function-code --function-name beeline-routing-suggestions --s3-bucket beeline-analytics --s3-key routing-lambda-assembly-1.0.jar
+aws s3 cp ./routing-lambda/target/scala-2.12/routing-lambda-assembly-1.0.jar s3://beeline-routing/
+
+create_lambda || true
+aws lambda update-function-code --function-name beeline-routing-suggestions --s3-bucket beeline-routing --s3-key routing-lambda-assembly-1.0.jar
 
