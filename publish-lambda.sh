c44fbeacb7f986f6b90f91828d2536c5f597edb7
daniel sim <daniel@LAPTOP-VIQA6AQL.localdomain>
2018-09-27 19:00:38 +0800

    maximize performance and lambda exec time

--- publish-lambda.sh
+++ publish-lambda.sh
@@ -7,8 +7,8 @@ create_lambda () {
         --role arn:aws:iam::882000534153:role/service-role/beeline-routing \
         --handler 'sg.beeline.lambda.SuggestRouteHandler::handle' \
         --code '{"S3Bucket":"beeline-routing","S3Key":"routing-lambda-assembly-1.0.jar"}' \
-        --timeout 60 \
-        --memory-size 1024
+        --timeout 299 \
+        --memory-size 3000
 }
 
 sbt routingLambda/assembly
